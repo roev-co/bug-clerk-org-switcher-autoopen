9a3018b2bf72e7dbd76917fcf60806ad088ff54f
Artur Bodera <abodera@gmail.com>
2024-09-26 16:14:11 +1000

    Add repro code for bug.
    https://discord.com/channels/856971667393609759/1285052636265058354

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,14 @@
+import { OrganizationSwitcher, SignedIn, SignedOut } from "@clerk/nextjs";
+
 export default function Home() {
-  return <div>Home Page</div>;
+  return <>
+    <SignedOut>
+      <h1>Sign in first to see the issue ...</h1>
+    </SignedOut>
+    <SignedIn>
+      <h1>Below is an OrganizationSwitcher that is open by default</h1>
+      <OrganizationSwitcher defaultOpen={true}/>
+    </SignedIn>
+  </>
+;
 }
